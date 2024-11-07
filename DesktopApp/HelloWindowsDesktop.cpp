539132c5e917375eb9abf5edcde1a514990289af
Evert Ingwersen <everti2000@gmail.com>
2024-11-07 16:08:24 +0100

    started on WND class for defining window

--- DesktopApp/HelloWindowsDesktop.cpp
+++ DesktopApp/HelloWindowsDesktop.cpp
@@ -28,4 +28,26 @@ int WINAPI WinMain(
 	_In_ int nCmdShow
 )
 
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+
+
+
+
+
+
+}
 
