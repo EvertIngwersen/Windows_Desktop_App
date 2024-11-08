8ab3ea30d5205597c5656e334c9db04aeceb47e7
Evert Ingwersen <everti2000@gmail.com>
2024-11-08 11:49:47 +0100

    new variables defined

--- Console_app_files/Console_app_files/Console_app_files.cpp
+++ Console_app_files/Console_app_files/Console_app_files.cpp
@@ -1,20 +1,32 @@
-// Console_app_files.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// first program
 
 #include <iostream>
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << "Hello \n";
+	cout << "Extra onzin";
+
+	int a, b;
+	int result;
+
+	a = 5;
+	b = 7;
+	result = a - b;
+
+	cout << "\n";
+	cout << result;
+
+	return 0;
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+// variable decleration 
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int x = 5; // c- like: type indentifer = initial_value
+int y(0);
+int z{ 0 };
+
+
